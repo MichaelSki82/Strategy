c3c7e95bd87d31dd56d4e9a25b38c3df5802481c
MichaelSki82 <romantik1001@yandex.ru>
2022-03-27 15:46:38 +0300

    HW1
    Outline and UI

---
+++ StrategySkipin/Assets/Scripts/UserControlSystem/UI/Presenter/WorkWithOutline.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Outline))]
+public class WorkWithOutline : MonoBehaviour
+{
+    private Outline _outline;
+
+    private void OnEnable()
+    {
+        _outline = GetComponent<Outline>();
+        Debug.Log("close outline");
+        _outline.OutlineWidth = 0;
+
+    }
+
+    public void OutlineActivation()
+    {
+        _outline.OutlineWidth = 6;
+    }
+
+    public void OutlineDeactivation()
+    {
+        _outline.OutlineWidth = 0;
+    }
+}
